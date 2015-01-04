494daf50e543ed68f2f342037a68c663ac810d49
Felix Amerbauer <felix@amerbauer.at>
2015-01-04 21:31:44 +0100

    added language switcher

--- js/controllers.js
+++ js/controllers.js
@@ -19,3 +19,13 @@ controllers.controller('CarouselCtrl', ['$scope', '$routeParams', 'myService',
             $scope.pictures = myService.imageCategories[$routeParams.imgId];
         }]
 );
+
+controllers.controller('LangCtrl', function ($scope, $translate) {
+    $scope.changeLang = function (key) {
+        $translate.use(key).then(function (key) {
+            console.log("Sprache zu " + key + " gewechselt.");
+        }, function (key) {
+            console.log("Irgendwas lief schief.");
+        });
+    };
+});
--- js/i18n.js
+++ js/i18n.js
@@ -1,11 +1,15 @@
 angular.module('app').config(function ($translateProvider) {
     $translateProvider.translations('de',
         {
-            MENU_QA: 'Fragen & Antworten'
+            MENU_QA: 'Fragen & Antworten',
+            MENU_i18n_en: 'Englisch',
+            MENU_i18n_de: 'Deutsch'
         });
     $translateProvider.translations('en',
         {
-            MENU_QA: 'Q & A'
+            MENU_QA: 'Q & A',
+            MENU_i18n_en: 'English',
+            MENU_i18n_de: 'German'
         });
 
     // Nicht vergessen: die Standardsprache
