6cc9b79a49eea9b204909f087dc9afa14ff116e5
Felix Amerbauer <felix@amerbauer.at>
2015-01-21 21:33:33 +0100

    wip, many small changes

--- js/i18n.js
+++ js/i18n.js
@@ -3,13 +3,17 @@ angular.module('app').config(function ($translateProvider) {
         {
             MENU_QA: 'Fragen & Antworten',
             MENU_i18n_en: 'Englisch',
-            MENU_i18n_de: 'Deutsch'
+            MENU_i18n_de: '> Deutsch',
+            IMG_about: 'img_static/about.png',
+            IMG_contact: 'img_static/contact.png'
         });
     $translateProvider.translations('en',
         {
             MENU_QA: 'Q & A',
-            MENU_i18n_en: 'English',
-            MENU_i18n_de: 'German'
+            MENU_i18n_en: '> English',
+            MENU_i18n_de: 'German',
+            IMG_about: 'img_static/about_en.png',
+            IMG_contact: 'img_static/contact_en.png'
         });
 
     // Nicht vergessen: die Standardsprache
