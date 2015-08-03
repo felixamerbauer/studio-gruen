5abbdffc876c285439c417a1cb7297a4da5b0387
Felix Amerbauer <felix@amerbauer.at>
2015-08-03 16:19:33 +0200

    added photos to start page

--- js/controllers.js
+++ js/controllers.js
@@ -5,8 +5,14 @@ var controllers = angular.module('controllers', []);
 
 controllers.controller('MenuCtrl', ['$scope', '$http', 'myService',
     function ($scope, $http, myService) {
-        // console.log("MenuCtrl");
+        console.log("MenuCtrl");
         $scope.categories = myService.categories;
+        $scope.pictures = [
+            'img/Baby_01-Neugeborenenfotografie-Wien.jpg',
+            'img/Baby_04-Neugeborenenfotografie-Stillen-Wien.jpg',
+            'img/Familie_01-Familienfotografie-Natur-Mostviertel.jpg',
+            'img/Portrait_03-Garten-Eier-Selbstversorgung-Niederoesterreich.jpg',
+            'img_static/Schwangerschaft_13-Babybauch-Natur-Wien.jpg'];
     }]);
 
 controllers.controller('DummyCtrl', [,
